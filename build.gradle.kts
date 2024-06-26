d35bbad98795dc0b115939e84ba3dffdf80b21e3
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2024-06-26 10:38:19 +0300

    javalin connected

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    application
+    id("io.freefair.lombok") version "8.4"
 }
 
 group = "org.example"
@@ -9,9 +11,17 @@ repositories {
     mavenCentral()
 }
 
+application {
+    mainClass.set("org.example.hexlet.HelloWorld")
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("gg.jte:jte:3.1.9")
 }
 
 tasks.test {
