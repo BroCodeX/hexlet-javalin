28997284a8eac315a73e67153c5584de3ab24d37
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2024-07-01 12:51:57 +0300

    JTE layouts

--- jte-classes/gg/jte/generated/ondemand/JteindexGenerated.java
+++ jte-classes/gg/jte/generated/ondemand/JteindexGenerated.java
@@ -2,35 +2,29 @@ package gg.jte.generated.ondemand;
 import org.example.hexlet.DTO;
 public final class JteindexGenerated {
 	public static final String JTE_NAME = "index.jte";
-	public static final int[] JTE_LINE_INFO = {0,0,1,1,1,8,8,8,8,9,9,11,11,12,12,14,14,14,14,14,14,14,15,15,15,17,17,18,18,20,20,20,1,1,1,1};
-	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, DTO push) {
-		jteOutput.writeContent("\n<html>\n<head>\n    <title>Хекслет</title>\n</head>\n<body>\n<h1>");
-		jteOutput.setContext("h1", null);
-		jteOutput.writeUserContent(push.getHeader());
-		jteOutput.writeContent("</h1>\n");
-		if (push.getModels().isEmpty()) {
-			jteOutput.writeContent("\n    <p>Пока не добавлено ни одного курса</p>\n");
-		} else {
-			jteOutput.writeContent("\n    ");
-			for (var model : push.getModels()) {
-				jteOutput.writeContent("\n        <div>\n            <h2><a href=\"/courses/");
-				jteOutput.setContext("a", "href");
-				jteOutput.writeUserContent(model.getId());
-				jteOutput.setContext("a", null);
-				jteOutput.writeContent("\">");
-				jteOutput.setContext("a", null);
-				jteOutput.writeUserContent(model.getName());
-				jteOutput.writeContent("</a></h2>\n            <p>");
+	public static final int[] JTE_LINE_INFO = {0,0,1,1,1,3,3,7,7,8,8,8,9,9,9,9,11,11,11,11,13,13,13,14,14,14,1,1,1,1};
+	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, DTO dto) {
+		jteOutput.writeContent("\n");
+		gg.jte.generated.ondemand.layout.JtepageGenerated.render(jteOutput, jteHtmlInterceptor, new gg.jte.html.HtmlContent() {
+			public void writeTo(gg.jte.html.HtmlTemplateOutput jteOutput) {
+				jteOutput.writeContent("\n    <p>Welcome, ");
 				jteOutput.setContext("p", null);
-				jteOutput.writeUserContent(model.getDescription());
-				jteOutput.writeContent("</p>\n        </div>\n    ");
+				jteOutput.writeUserContent(dto.getModels().get(0).getName());
+				jteOutput.writeContent(".</p>\n");
 			}
-			jteOutput.writeContent("\n");
-		}
-		jteOutput.writeContent("\n</body>\n</html>");
+		}, new gg.jte.html.HtmlContent() {
+			public void writeTo(gg.jte.html.HtmlTemplateOutput jteOutput) {
+				jteOutput.writeContent("\n    <a href=\"http://localhost:7070/\">Главная</a>\n");
+			}
+		}, new gg.jte.html.HtmlContent() {
+			public void writeTo(gg.jte.html.HtmlTemplateOutput jteOutput) {
+				jteOutput.writeContent("\n    <p>https://github.com/BroCodeX</p>\n");
+			}
+		});
+		jteOutput.writeContent("\n");
 	}
 	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
-		DTO push = (DTO)params.get("push");
-		render(jteOutput, jteHtmlInterceptor, push);
+		DTO dto = (DTO)params.get("dto");
+		render(jteOutput, jteHtmlInterceptor, dto);
 	}
 }
--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -2,12 +2,9 @@ package org.example.hexlet;
 
 import io.javalin.Javalin;
 import io.javalin.http.NotFoundResponse;
-import io.javalin.validation.Validator;
-import org.example.hexlet.Model;
-import org.example.hexlet.DTO;
+
 
 import java.util.List;
-import java.util.Optional;
 import io.javalin.rendering.template.JavalinJte;
 
 import static io.javalin.rendering.template.TemplateUtil.model;
@@ -21,8 +18,8 @@ public class HelloWorld {
         });
         app.get("/users/{id}", ctx -> {
             Integer id = ctx.pathParamAsClass("id", Integer.class).getOrDefault(0);
-            if (id == 1) {
-                Model model = new Model("Java #1", "New Course desc");
+            if (id != 0) {
+                Model model = new Model("BroCodeX", "New Course desc");
                 model.setId(1L);
                 DTO dto = new DTO(List.of(model), "This is My header");
                 ctx.render("index.jte", model("dto", dto));
